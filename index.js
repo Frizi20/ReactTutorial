3b914af316221aa7db5a5e1b91c03091a7c4af90
unknown <frizi20@yahoo.com>
2022-08-24 12:33:33 +0300

    upload project

---
+++ index.js
@@ -0,0 +1,7 @@
+const root = document.getElementById('root')
+
+const element = document.createElement('h2')
+element.classList.add('header')
+element.append('I\'m learning React')
+// root.append(elContent)
+root.append(element)
