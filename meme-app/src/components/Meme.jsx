26702c9fac2a3d5210971747af6a85d1e9415088
Frizi <frizi20@yahoo.com>
2022-09-05 01:53:13 +0300

    finalised part 3

--- meme-app/src/components/Meme.jsx
+++ meme-app/src/components/Meme.jsx
@@ -1,10 +1,18 @@
 import memesData from '../memesData'
-import memeData from '../memesData'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Meme() {
 
-    const [memeUrl, setMemeUrl] = useState(null)
+
+
+    const [meme,setMeme] = useState({
+        topText:'One does not simply',
+        bottomText:'walk into mordor',
+        randomImage:'http://i.imgflip.com/1bij.jpg'        
+    })
+    const [allMemes, setAllMemes] =useState([])
+    
+
 
     function random(nr){
         return Math.floor(Math.random() * nr + 1)
@@ -13,11 +21,38 @@ export default function Meme() {
 
     function selectRandomMeme(e){
         e.preventDefault()
-        const {data} = memesData
-        const {memes} = data
-        const randMeme = memes[random(memes.length)]
-        setMemeUrl(randMeme.url)
+        console.log(meme)
+        
+        
+        const url = allMemes[random(allMemes.length)].url
+        setMeme(prevMeme =>({
+            ...prevMeme,
+            randomImage:url
+        }))
     }
+
+    function handleChange(e){
+        const {name, value} = e.target
+        setMeme(prevMeme=>{
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+
+
+    }
+
+    useEffect(()=>{
+        fetch('https://api.imgflip.com/get_memes')
+        .then(res =>res.json())
+        .then(resData=>{
+            // const {memes} = data
+            const memes = resData.data.memes
+            console.log(memes)
+            setAllMemes(memes)
+        })
+    },[])
 
 
     // const result = useState('hello')
@@ -29,16 +64,33 @@ export default function Meme() {
         <div className="main">
             <form action="">
                 <div className="form-inputs">
-                    <input type="text" name="" placeholder="top text" id="" />
-                    <input type="text" name="" placeholder="bottom text" id="" />
+
+                    <input 
+                    type="text" 
+                    name="topText"
+                    placeholder="top text" 
+                    value={meme.topText}
+                    onChange={handleChange}
+                    id="" />
+
+                    <input 
+                    type="text" 
+                    name="bottomText"
+                    placeholder="bottom text"
+                    value={meme.bottomText}
+                    onChange={handleChange} 
+                    id="" />
+
                 </div>
                 <button onClick={selectRandomMeme}>
                     Get a new meme Image
                 </button>
             </form>
             {   
-    memeUrl && <div className="meme-img">
-                    <img src={memeUrl} alt="" />
+                 <div className="meme-img">
+                    <img src={meme.randomImage} alt="" />
+                    <h2 className="meme--text top"> {meme.topText} </h2>
+                    <h2 className="meme--text bottom"> {meme.bottomText} </h2>
                 </div>
             }
             
